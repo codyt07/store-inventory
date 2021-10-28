a3d7615463dbee8c423b68f3757a94c527d7e337
Cody Travis <89680528+codyt07@users.noreply.github.com>
2021-10-28 16:17:22 -0500

    Finished Product Modification Page

--- app.py
+++ app.py
@@ -12,18 +12,23 @@ def view_summary():
     try:
         view_product = session.query(Product).filter(
             Product.id == product_selection).first()
+        easy_date = view_product.updated.strftime('%m-%d-%Y')
         selection = input(f'''\nProduct Information for Product ID {product_selection}
                 \r* Product Name: {view_product.name}
                 \r* Product Price: ${view_product.price / 100}
                 \r* Product Quantity: {view_product.quantity}
-                \r* Information Last Updated: {view_product.updated}
+                \r* Information Last Updated: {easy_date}
                 \nEnter A To Return To Main Menu
                 \rEnter B To Go Back To Product ID Screen
+                \rEnter C To Update Product
                 \rCommand: ''')
         if selection.lower() == "a":
             menu()
         elif selection.lower() == "b":
             view_summary()
+        elif selection.lower() == 'c':
+            update_id = view_product.id
+            update_product(update_id)
         else: 
             input('''\nValid input not Entered
                     \rPress Enter to return main Menu''')
@@ -38,7 +43,7 @@ def update_product(update_id):
         Product.id == update_id).first()
     updating = True
     while updating == True:
-        print(f'Updating: {to_update.name}')
+        print(f'\n**** Updating: {to_update.name}')
         command = input('''\nEnter Command From The List
                         \rEnter A To Update the Product Name
                         \rEnter B To Update Products Price
@@ -47,6 +52,7 @@ def update_product(update_id):
                         \rEnter E To Delete Product
                         \rEnter F To Cancel and Return to Main Menu
                         \rCommand: ''')
+        #Update Name
         if command.lower() == 'a':
             new_name = True
             while new_name:
@@ -68,11 +74,11 @@ def update_product(update_id):
                     pass
                 elif selection.lower() == 'c':
                     new_name = False
-
+        #Update Price
         elif command.lower() == 'b':
             new_price = True
             while new_price:
-                product_price = input(f'''\nCurrent Price: {to_update.price/ 100}
+                product_price = input(f'''\nCurrent Price: {to_update.price / 100}
                             \rPlease Enter A New Price Below
                             \r** Do Not Use a Currency Symbol **
                             \rNew Price: ''')
@@ -89,23 +95,81 @@ def update_product(update_id):
                     print('''\n Product Price Updated!
                             \rReturning Back To Sub Menu''')
                     time.sleep(1.5)
-
                 elif confirm.lower() == 'b':
                     pass
                 elif confirm.lower() == 'c':
                     new_price = False
-        
+        #Update Quantity
         elif command.lower() == 'c':
-            pass
+            new_quantity = True
+            while new_quantity:
+                product_quantity = input(f'''\nCurrent Quanitity: {to_update.quantity}
+                            \rEnter a New Quantity: ''')
+                update_quantity = quantity_checker(product_quantity)
+                confirm = input(f'''\nConfirming New Quanitity: {product_quantity}
+                                \rEnter A To Confirm
+                                \rEnter B To Enter a New Quantity
+                                \rEnter C To Go Back to Product Modification Menu
+                                \rCommand: ''')
+                if confirm.lower() == 'a':
+                    to_update.quantity = update_quantity
+                    session.commit()
+                    new_quantity = False
+                    print('''\nProduct Quantity Updated!
+                        \rReturning Back to Sub Menu''')
+                    time.sleep(1.5)
+                    
+                elif confirm.lower() == 'b':
+                    pass
+                elif confirm.lower() == 'c':
+                    new_quantity = False
+        #Update Date
         elif command.lower() == 'd':
-            pass
+            new_date = True
+            easy_date2 = to_update.updated.strftime('%m-%d-%Y')
+            while new_date:
+                product_date = input(f'''\nCurrent Product Date: {easy_date2}
+                                \rEnter A New Date (MM-DD-YYYY): ''')
+                update_date = date_checker(product_date)
+                easy_date = update_date.strftime('%m-%d-%Y')
+                confirm = input(f'''\nConfirming New Date: {easy_date}
+                                \rEnter A To Confirm
+                                \rEnter B To Enter a New Date
+                                \rEnter C To Go Back to Product Modification Menu
+                                \rCommand: ''')
+                if confirm.lower() == 'a':
+                    to_update.updated = update_date
+                    session.commit()
+                    new_date = False
+                    print('''\nProduct Date Updated!
+                        \rReturning Back to Sub Menu''')
+                    time.sleep(1.5)
+                elif confirm.lower() == 'b':
+                    pass
+                elif confirm.lower() == 'c':
+                    new_date = False
+        #Delete Product
         elif command.lower() == 'e':
-            pass
+            deletion = True
+            while deletion:
+                confirm = input(f'''\nDeleting: {to_update.name}
+                            \rEnter A To Confirm
+                            \rEnter B To Cancel
+                            \rCommand: ''')
+                if confirm.lower() == 'a':
+                    session.delete(to_update)
+                    session.commit()
+                    deletion = False
+                    print("\nItem Deleted! Returning Back To Main Menu")
+                    time.sleep(1.5)
+                    menu()
+                elif confirm.lower() == 'b':
+                    deletion = False
+        #Return To Menu
         elif command.lower() == 'f':
             updating = False
             print("Returning to Main Menu")
             menu()
-
 
 def price_checker(product_price):
     price_error = True
